10e289ef12d8663f99ec2f41fcbfcb9afea9500c
WagnerRamos1 <wagner.ramos@academico.ifpb.edu.br>
2026-09-09 18:23:44 -0300

    feat: Adiciona o persistence.xml e o JPAUtil

---
+++ src/main/java/com/ifpb/hard_zone/util/JPAUtil.java
@@ -0,0 +1,15 @@
+package com.ifpb.hard_zone.util;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JPAUtil {
+
+    private static final EntityManagerFactory FACTORY =
+            Persistence.createEntityManagerFactory("hardzone-pu");
+
+    public static EntityManager getEntityManager() {
+        return FACTORY.createEntityManager();
+    }
+}
