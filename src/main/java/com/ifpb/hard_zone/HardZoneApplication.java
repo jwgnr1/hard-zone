1e4a68b600af87485426ad2f61b1227e3a7657f4
WagnerRamos1 <wagner.ramos@academico.ifpb.edu.br>
2026-09-09 11:09:04 -0300

    feat: Estrutura inicial gerada pelo Spring Initializr

---
+++ src/main/java/com/ifpb/hard_zone/HardZoneApplication.java
@@ -0,0 +1,13 @@
+package com.ifpb.hard_zone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HardZoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifpb/hard_zone/HardZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifpb.hard_zone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
